1ce9c74c35b857ff6e1f04e54397f35fdd2ea148
admin <admin>
2017-10-09 18:21:17 -0400

    need to keep working on this

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -22,8 +22,34 @@ var printProducts = function() {
     if(err){throw err;}
    console.log(results);
 });
+/*  connection.query("select * from products", function(err, results){
+    if(err){throw err;}
+
+   console.log(results);
+
+    var resultsArray = [];
+     for (var i = 0; i < results.length; i++) {
+        resultsArray[i] = results[i].names;
+     };
+
+   inquirer.prompt([
+      {
+        type: "list",
+        name: "options",
+        message: "Here are your options",
+        choices: [resultsArray[0], resultsArray[1], resultsArray[2]]
+      }
+    ]).then(function(answers) {
+      console.log("I'm working")
+      for (var i = 0; i < results.length; i++) {
+        if (results[i].names === answers.options) {
+          compareBids(results[i].id, results[i].price)
+        };
+      };
+    });
+  });
 }
-/*
+*/
 var askUser = function() {
 	inquirer.prompt([
       {
@@ -34,7 +60,51 @@ var askUser = function() {
 
    console.log("I'm working");
    console.log(answers.userChoice);
-  
+  switch(answers.userChoice) {
+  	case '1':
+  		id = 1;
+  		console.log(id);
+  		break;
+  	case '2':
+  		id = 2;
+  		console.log(id);
+  		break;
+  	case '3':
+  		id = 3;
+  		console.log(id);
+  		break;
+  	case '4':
+  		id = 4;
+  		console.log(id);
+  		break;
+  	case '5':
+  		id = 5;
+  		console.log(id);
+  		break;
+  	case '6':
+  		id = 6;
+  		console.log(id);
+  		break;
+  	case '7':
+  		id = 7;
+  		console.log(id);
+  		break;
+  	case '8':
+  		id = 8;
+  		console.log(id);
+  		break;
+  	case '9':
+  		id = 9;
+  		console.log(id);
+  		break;
+  	case '10':
+  		id = 10;
+  		console.log(id);
+  		break;
+  	default:
+  		console.log("Invalid input");
+  		break;
+  }
    
+})
 }
-} */
